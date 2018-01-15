71394bf5ddaef91875d6b15f2be9297d5bcc185e
Gary Jennings <gary.jennings@rallyengine.com>
2018-01-14 17:59:55 -0700

    first commit

---
+++ MobileMenu/app.js
@@ -0,0 +1,27 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// 1. Get HTML Elements
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+var openButton      = document.querySelector('#open-mobile-menu-button');
+var closeButton     = document.querySelector('#close-mobile-menu-button');
+var mobileMenu      = document.querySelector('#mobile-menu');
+
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// 2. Define Abilities (Functions)
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+function openMenu() {
+    mobileMenu.className = 'menu open';
+}
+
+function closeMenu() {
+    mobileMenu.className = 'menu';
+}
+
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// 3. Wire everything up
+// * * * * * * * * * * * * * * * * * * * * * * * * * * *
+openButton.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);

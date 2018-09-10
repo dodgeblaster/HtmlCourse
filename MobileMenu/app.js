8317ef431516447c97a1a6ffb9372bbc6500e041
Gary Jennings <garysjennings@gmail.com>
2018-09-09 23:04:24 -0600

    Update app.js

--- MobileMenu/app.js
+++ MobileMenu/app.js
@@ -1,15 +1,11 @@
-// * * * * * * * * * * * * * * * * * * * * * * * * * * *
 // 1. Get HTML Elements
-// * * * * * * * * * * * * * * * * * * * * * * * * * * *
 var openButton      = document.querySelector('#open-mobile-menu-button');
 var closeButton     = document.querySelector('#close-mobile-menu-button');
 var mobileMenu      = document.querySelector('#mobile-menu');
 
 
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * *
 // 2. Define Abilities (Functions)
-// * * * * * * * * * * * * * * * * * * * * * * * * * * *
 function openMenu() {
     mobileMenu.className = 'menu open';
 }
@@ -20,8 +16,6 @@ function closeMenu() {
 
 
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * *
 // 3. Wire everything up
-// * * * * * * * * * * * * * * * * * * * * * * * * * * *
 openButton.addEventListener('click', openMenu);
 closeButton.addEventListener('click', closeMenu);
